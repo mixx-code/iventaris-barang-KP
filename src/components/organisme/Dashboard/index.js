acdbc563d75fd39fda6bfac4cdccee26b719d92e
mixx-code <rfebriansyah385@gmail.com>
2023-06-10 14:27:21 +0700

    buat input untuk serach dan proses nya

--- src/components/organisme/Dashboard/index.js
+++ src/components/organisme/Dashboard/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "../../atoms";
+import { Button, Input } from "../../atoms";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { CardItem, TabelBarangKeluar, TabelBarangMasuk } from "../../molecules";
@@ -8,34 +8,53 @@ const Dashboard = () => {
   const Api = "https://iventaris-barang-api.cyclic.app/";
   //state
   const [counter, setCounter] = useState(1);
-
   const [dataItem, setDataItem] = useState([]);
+  const [query, setQuery] = useState("");
   const [page, setPage] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  console.log("ini query", query);
   //akhir state
-  let totalPage = Math.ceil(page.total_data / page.per_page);
+  let totalPage = 0;
+  if (page && page.total_data && page.per_page) {
+    totalPage = Math.ceil(page.total_data / page.per_page);
+  }
 
   let halaman = counter;
 
   useEffect(() => {
     setIsLoading(true); // set loading menjadi true saat permintaan data dimulai
-    axios
-      .get(
-        `${Api}/v1/iventaris/items?page=${halaman}&perPage=10` ||
-          `http://localhost:4000/v1/iventaris/items?page=${halaman}&perPage=10`
-      )
-      .then((response) => {
+
+    const fetchData = async () => {
+      try {
+        setIsLoading(true); // set loading menjadi true saat permintaan data dimulai
+
+        let apiUrl = `${Api}/v1/iventaris/items?page=${halaman}&perPage=10`;
+
+        if (query) {
+          apiUrl =
+            `${Api}/v1/iventaris/search?query=${encodeURIComponent(
+              query
+            )}&page=${halaman}&perPage=10` ||
+            `http://localhost:4000/v1/iventaris/search?query=${encodeURIComponent(
+              query
+            )}&page=${halaman}&perPage=10`;
+        }
+
+        // Jeda selama 2 detik sebelum permintaan ke API dilakukan
+
+        const response = await axios.get(apiUrl);
         const responseAPI = response.data;
         setDataItem(responseAPI.data);
         setPage(responseAPI);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.error(error);
+      } finally {
         setIsLoading(false); // set loading menjadi false ketika permintaan data selesai
-      });
-  }, [halaman]);
+      }
+    };
+
+    fetchData();
+  }, [halaman, query]);
 
   const previous = () => {
     setCounter(counter <= 1 ? 1 : counter - 1);
@@ -46,25 +65,39 @@ const Dashboard = () => {
   };
   return (
     <div className="w-full bg-custom-putih flex flex-col  h-max mt-14 ml-80 ">
-      <h1 className="text-3xl ml-24 mt-9 font-semibold z-10">Barang</h1>
+      <div className="flex justify-between mr-24">
+        <h1 className="text-3xl ml-24 mt-9 font-semibold z-10">Barang</h1>
+        <div className="w-80 flex justify-center items-center box-border">
+          <label className="text-lg mr-2 text-gray-700">Search</label>
+          <Input
+            type="text"
+            value={query}
+            placeholder="Search Barang"
+            onChange={(e) => setQuery(e.target.value)}
+            className=" shadow appearance-none border rounded-r-none w-80 py-2 px-4 text-gray-700 leading-tight hover:cursor-text focus:outline-none focus:shadow-outline mt-2"
+          />
+        </div>
+      </div>
 
       {isLoading ? (
         <div className="mx-auto -mt-10 grid grid-cols-2 gap-x-40 gap-y-5 relative">
-          <CardItem />
-          <CardItem />
+          <CardItem nama_item="loading . . ." total_stok="loading . . ." />
+          <CardItem nama_item="loading . . ." total_stok="loading . . ." />
         </div>
       ) : (
         <div className=" mx-auto -mt-10 grid grid-cols-2 gap-x-40 gap-y-5 relative">
-          {dataItem.map((item) => {
-            return (
+          {dataItem.length > 0 ? (
+            dataItem.map((item) => (
               <CardItem
                 key={item._id}
                 nama_item={item.nama_item}
                 total_stok={item.total_stok}
                 id={item._id}
               />
-            );
-          })}
+            ))
+          ) : (
+            <h1 className="text-2xl mt-10 mx-auto">Data tidak ditemukan</h1>
+          )}
           <div className="flex  absolute -bottom-16 right-0">
             <Button
               label="Seblumnya"
@@ -72,7 +105,9 @@ const Dashboard = () => {
               onClick={previous}
             />
             <p className="text-xl px-6">
-              {page.current_page} / {totalPage}
+              {page && page.current_page
+                ? `${page.current_page} / ${totalPage}`
+                : ""}
             </p>
             <Button
               label="Selanjutnya"
